05f7428cd523b0f7c9b4db1a5266e2da6e938bd3
kichyan <kikikunsang@gmail.com>
2018-10-03 17:58:19 +1300

    test

---
+++ Assets/Old Scripts/ColourController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourController : MonoBehaviour {
+    public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other){
+        player.colourStatus = 1;
+        Debug.Log(player.colourStatus);
+    }
+}
