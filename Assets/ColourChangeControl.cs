ae7ad906c993c77c0e10fc7d295d85fc34ea1b9d
iodachi <a547601908@gmail.com>
2018-10-04 11:27:49 +1300

    Revert "Merge branch 'master' of https://github.com/Iodachi/Revive"
    This reverts commit f474566753949486c8de3640a3bc7b9a62af30a5, reversing
    changes made to 0dc13f3ad58e43ddbdbbf8b299a4b397de73dee7.

--- Assets/ColourChangeControl.cs
+++ Assets/ColourChangeControl.cs
@@ -1,4 +1,5 @@
-﻿﻿using System.Collections;
+<<<<<<< HEAD
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,3 +23,29 @@ public class ColourChangeControl : MonoBehaviour {
         player.SetColour(1);
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChangeControl : MonoBehaviour {
+
+    public PlayerControl player;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered");
+        player.SetColour(1);
+    }
+}
+>>>>>>> 7cb96a7c3d01e6536c2b1b3a7f2517be11a0872d
--- Assets/Old Scripts/ColourController.cs
+++ Assets/Old Scripts/ColourController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,3 +21,27 @@ public class ColourController : MonoBehaviour {
         Debug.Log(player.colourStatus);
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourController : MonoBehaviour {
+    public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other){
+        player.colourStatus = 1;
+        Debug.Log(player.colourStatus);
+    }
+}
+>>>>>>> 7cb96a7c3d01e6536c2b1b3a7f2517be11a0872d
