4eb92c9527f9ea28310bc9adb5cc3b73c9b31d2d
iodachi <a547601908@gmail.com>
2018-10-08 19:20:19 +1300

    camera on player

--- Assets/Old Scripts/ColourController.cs
+++ Assets/Old Scripts/ColourController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,27 +20,3 @@ public class ColourController : MonoBehaviour {
         Debug.Log(player.colourStatus);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ColourController : MonoBehaviour {
-    public PlayerController player;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void OnTriggerEnter (Collider other){
-        player.colourStatus = 1;
-        Debug.Log(player.colourStatus);
-    }
-}
->>>>>>> 7cb96a7c3d01e6536c2b1b3a7f2517be11a0872d
