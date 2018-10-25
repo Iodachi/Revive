62e3afd20d9ed326354a7f57266c638d0fe7d5d5
iodachi <a547601908@gmail.com>
2018-10-26 12:49:28 +1300

    #7 light up when get close

---
+++ Assets/Scripts/LightChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChange : MonoBehaviour {
+    Light lt;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lt = GetComponent<Light>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            //if(Input.GetKeyDown("e"))
+                lt.intensity = 2;
+        }
+    }
+}
