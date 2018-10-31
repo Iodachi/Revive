a280d07bccfe5222869573a97455d4b4f49f5bca
Guest User <Guest@wg401-b-05-06.mddn.vuw.ac.nz>
2018-10-31 18:27:32 +1300

    text instructions

--- Assets/Scripts/NPCDialogTrigger.cs
+++ Assets/Scripts/NPCDialogTrigger.cs
@@ -6,28 +6,57 @@ using UnityEngine.UI;
 public class NPCDialogTrigger : MonoBehaviour {
 
     public Dialog dialog;
-    public DialogManager manager;
     public Text instruction;
+    public Text nameText;
+    public Text dialogText;
+
+    private bool entered;
+    private bool isIn;
 
     void Start(){
-        manager = FindObjectOfType<DialogManager>();
         instruction.gameObject.SetActive(false);
+        entered = false;
+    }
+
+    // kinda hard coded since getKey not working in the startdialog method.
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.V) && isIn)
+        {
+            dialogText.text = dialog.sentences[1];
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player"){
-            Debug.Log("entered");
-            manager.StartDialog(dialog);
+        //only show when first entered
+        if(other.tag == "Player" && !entered){
+            StartDialog();
             instruction.gameObject.SetActive(true);
+            entered = true;
+            isIn = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if(other.tag == "Player"){
-            FindObjectOfType<DialogManager>().EndDialog();
+            EndDialog();
             instruction.gameObject.SetActive(false);
+            isIn = false;
         }
+    }
+
+    public void StartDialog()
+    {
+        nameText.text = dialog.name;
+        dialogText.text = dialog.sentences[0];
+
+    }
+
+    public void EndDialog()
+    {
+        nameText.text = "";
+        dialogText.text = "";
     }
 }
